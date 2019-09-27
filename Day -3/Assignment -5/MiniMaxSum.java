4d1045f368167741696b6b3dd59586e1c61240a2
Ravi1004 <viswanadhularavi@msitprogram.net>
2019-09-27 11:26:20 +0530

    "Assignmanet 5

---
+++ Day -3/Assignment -5/MiniMaxSum.java
@@ -0,0 +1,91 @@
+/**
+ * Given an array of elements, return the String that has two values separated
+ * by commans. The first value is the sum of all the elements except minimum
+ * and the second value is the sum of all the elements except the maximum.
+ * 
+ * Input : {1,2,3,4,5};
+ * Output : 14,10
+ * 
+ * Input : {1,2}
+ * Output : 2,1
+ * 
+ * @author Siva Sankar
+ */
+import java.lang.*;
+import java.util.*;
+public class MiniMaxSum {
+
+    /**
+     * This method returns the minimum element in the array.
+     * @param arr, contains the elements in arr.
+     * @return the minimum element in the arr. If there are no elements, then
+     * it should return -1.
+     */
+    public static int min(int[] arr) {
+        //  Your code goes here...
+        int[] arrDup = arr ;
+        //System.out.println(Arrays.toString(arrDup));
+        int temp ;
+        for (int i = 0; i < arrDup.length ; i++){
+            for (int j = i + 1; j < arrDup.length ; j++){
+                if ((arrDup[i] > arrDup[j])) {
+                    temp = arrDup[i] ;
+                    arrDup[i] = arrDup[j] ;
+                    arrDup[j] = temp ;
+                }
+            }
+        }
+        return arrDup[0] ;
+    }
+
+    /**
+     * This method returns the maximum element in the array.
+     * @param arr, contains the elements in arr.
+     * @return the maximum element in the arr. If there are no elements, then
+     * it should return -1.
+     */
+    public static int max(int[] arr) {
+        //  Your code goes here...
+        int [] arrDup = arr ;
+        int temp ;
+        for (int i = 0; i < arrDup.length ; i++){
+            for (int j = i + 1; j < arrDup.length ; j++){
+                if ((arrDup[i] < arrDup[j])) {
+                    temp = arrDup[i] ;
+                    arrDup[i] = arrDup[j] ;
+                    arrDup[j] = temp ;
+                }
+            }
+        }
+        return arrDup[0];
+    }
+
+    /**
+     * This method returns the sum of all the elements in the array.
+     * @param arr, contains the elements in arr.
+     * @return the sum of all the elements in the arr. returns -1 if there are
+     * no elements.
+     */
+    public static int sum(int[] arr) {
+        //  Your code goes here...
+        int arrsum = 0 ;
+        for (int i = 0; i < arr.length; i++) {
+            arrsum = arrsum + arr[i] ;
+        }
+        return arrsum ;
+    }
+    
+    /**
+     * Make use of the functions defined for finding the 
+     * minimum element, maximum element and the sum of all the elements of teh 
+     * array arr.
+     * 
+     * @param arr, the input array.
+     * @return the string version of maxSum and minSum separated by commas.
+     */
+    public static String miniMaxSum(int[] arr) {
+        //  Your code goes here...
+        
+        return (sum(arr) - min(arr)) + "," + (sum(arr) - max(arr)) ;
+    }
+}
