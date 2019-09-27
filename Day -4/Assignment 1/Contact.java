6c45e9a455ee5f4a765921f57edea84feda2605d
Ravi1004 <viswanadhularavi@msitprogram.net>
2019-09-27 10:35:29 +0530

    "Assignment -1"

---
+++ Day -4/Assignment 1/Contact.java
@@ -0,0 +1,47 @@
+/**
+ * This class discusses about the Contact Object. The attributes of the contact object
+ * are as follows.
+ * name : Name of the person / friend.
+ * email : mail id of the person / friend.
+ * phoneNumber : Phone Number of the person / friend.
+ * 
+ * @author Siva Sankar
+ */
+
+public final class Contact {
+    private String name, email, phoneNumber;
+
+    public Contact(final String name, final String email, final String phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setEmail(final String email ) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setPhoneNumber(final String phn) {
+        this.phoneNumber = phn;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public String toString() {
+        return this.name + " " + this.email + " " + this.phoneNumber();
+    }
+}
