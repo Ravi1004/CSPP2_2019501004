bc16f2515fc022c82b1fe499a4b00ad26beab120
Ravi1004 <55682232+Ravi1004@users.noreply.github.com>
2020-03-27 03:20:15 +0000

    Update Card.java

--- Day -4/Assignment 2/Card.java
+++ Day -4/Assignment 2/Card.java
@@ -6,33 +6,58 @@ public class Card {
     public Card() {
 
     }
-    
+   /**
+    * 
+    * @param titleOfTheBook
+    * @param authorOftheBook
+    * @param subjectOfTheBook
+    */ 
     public Card(final String titleOfTheBook, final String authorOftheBook, final String subjectOfTheBook) {
         this.titleOfTheBook = titleOfTheBook;
         this.authorOfTheBook = authorOftheBook;
         this.subjectOfTheBook = subjectOfTheBook;
     }
-
+    /**
+     * 
+     * 
+     * @param titleOftheBook
+     */
     public void setTitleOfTheBook(final String titleOftheBook) {
         this.titleOfTheBook = titleOftheBook;
     }
-
+    /**
+     * 
+     * @return
+     *
+     */
     public String getTitleOfTheBook() {
         return this.titleOfTheBook;
     }
-
+    /**
+     * 
+     * @param authorOftheBook
+     */
     public void setAuthorOfTheBook(final String authorOftheBook) {
         this.authorOfTheBook = authorOftheBook;
     }
-
+    /**
+     * 
+     * @return
+     */
     public String getAuthorOfTheBook() {
         return this.authorOfTheBook;
     }
-
+    /**
+     * 
+     * @param subjectOftheBook
+     */
     public void setSubjectOfTheBook(final String subjectOftheBook) {
         this.subjectOfTheBook = subjectOftheBook;
     }
-
+    /**
+     * 
+     * @return
+     */
     public String getSubjectOfTheBook() {
         return this.subjectOfTheBook;
     }
