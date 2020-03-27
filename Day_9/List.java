6d898c5b18aeef0b968da9680df932d96a80bb82
Ravi1004 <55682232+Ravi1004@users.noreply.github.com>
2020-03-27 03:35:23 +0000

    Update List.java

--- Day_9/List.java
+++ Day_9/List.java
@@ -1,8 +1,11 @@
 import java.util.*;
-import java.lang.*;
 
+// this is a class
 public class List extends AbstractList{
-
+    /**
+     * This is an add method 
+     * @param item
+     */
     public void add(int item) {
         //Inserts the specified element at the end of the list.
         if (size <= list.length) {
